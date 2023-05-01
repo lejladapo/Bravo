467dd5a505e232f35b9d92e9120e00bc90db0b01
amars21 <amar.sose@hotmail.com>
2023-05-01 22:39:57 +0200

    Added navbar

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Navbar></Navbar>
       <Text>Bravo initial code!</Text>
       <StatusBar style="auto" />
     </View>
