5a979329e8f34989587692a1606e073258a46248
amars21 <amar.sose@hotmail.com>
2023-06-08 11:53:29 +0200

    Added login validation rules

--- components/Login.js
+++ components/Login.js
@@ -1,26 +1,43 @@
 import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
-import { View, TextInput, StyleSheet, Button } from 'react-native';
+import { View, TextInput, StyleSheet, Button, Text } from 'react-native';
 import Users from '../helpers/USERS';
 
 export default function Login({navigation}) {
 
     const [isAuthenticated, setIsAuthenticated] = useState(true);
+    const [firstName, setFirstName] = useState('');
+    const [email, setEmail] = useState('');
 
-    const handleFormSubmit = () => {
-        const user = Users.find(user => user.is_auth);
-        setIsAuthenticated(!!user);
-        if (user) {
-          navigation.navigate('ShoppingPage');
-        }
-      };
+  const handleFormSubmit = () => {
+    const user = Users.find(
+      (user) =>
+        user.first_name.toLowerCase() === firstName.toLowerCase() &&
+        user.email.toLowerCase() === email.toLowerCase()
+    );
+    setIsAuthenticated(!!user);
+    if (user) {
+      navigation.navigate('ShoppingPage');
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} placeholder="First Name" />
-      <TextInput style={styles.input} placeholder="Email" />
-      <Button style={styles.button} title="Log in" onPress={handleFormSubmit} />
+      <Text>Please log in to browse catalog</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="First Name"
+        value={firstName}
+        onChangeText={(text) => setFirstName(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        />
+      <Button style={styles.button} disabled={!firstName || !email} title="Log in" onPress={handleFormSubmit} />
       {!isAuthenticated && <Text style={styles.errorText}>Wrong first name and email combination.</Text>}
     </View>
   );
